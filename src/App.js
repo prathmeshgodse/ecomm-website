9fe4d693df54bfe330d5ab1bcdf18c9881c5f225
Prathmesh Godse <pgodse97@gmail.com>
2021-12-24 09:03:19 +0530

    Added Routing to Menu Item using withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
