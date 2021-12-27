9e7599daf00c7b0a682bab413e03469ee847c622
Prathmesh Godse <pgodse97@gmail.com>
2021-12-27 07:33:45 +0530

    Added Shop Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => {
   return (
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path="/" component={HomePage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
