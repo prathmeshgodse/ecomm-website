b387e1b5f5192e0b2fcb1ed03b039c17ab82c1cd
Prathmesh Godse <pgodse97@gmail.com>
2022-01-03 09:14:17 +0530

    Updated App to redirect site to Hompage if user is signed in

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from "./user.types";
+
 const INITAL_STATE = {
   currentUser: null,
 };
@@ -9,7 +11,7 @@ const INITAL_STATE = {
 
 const userReducer = (state = INITAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
