218d8e0ca636d06398f662eb99c06396c0051522
Prathmesh Godse <pgodse97@gmail.com>
2022-01-05 09:11:18 +0530

    Connected Cart Dropdown with Cart Items

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,10 +6,10 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
-        ? { ...cartItem, quantitiy: cartItem.quantitiy + 1 }
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
   }
 
-  return [...cartItems, { ...cartItemToAdd, quantitiy: 1 }];
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
